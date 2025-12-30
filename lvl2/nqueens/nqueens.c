44718a72190cc0a58bc9e70e412a0bbe42cc35b2
mnieto-m <majanoyieto@gmail.com>
2025-12-30 17:22:05 +0100

    mejore comenntarios

--- lvl2/nqueens/nqueens.c
+++ lvl2/nqueens/nqueens.c
@@ -6,7 +6,7 @@
 /*   By: mnieto-m <mnieto-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 15:47:01 by mnieto-m          #+#    #+#             */
-/*   Updated: 2025/12/26 18:14:52 by mnieto-m         ###   ########.fr       */
+/*   Updated: 2025/12/30 17:02:30 by mnieto-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 
 void print_posisiones(int nq, int *posisiones)
 {
-	int i = 0;
-	while(i < nq)
+	int i = 0; // declaramos un iterador 
+	while(i < nq)  //usaremos un bucle while para imprimir comparando el iterador y el numero de reinas
 	{
-		printf("%d", posisiones[i]);
-		if(i+ 1 != nq)
+		printf("%d", posisiones[i]); // imprimiremos cada valor del array de enteros en la posicion del iterador
+		if(i+ 1 != nq) //comprobaremos antes de iterar si es que el siguiente valor de i es distinto a nq para imprimir un espacio
 			printf(" ");
 		i++;
 	}
@@ -28,11 +28,11 @@ void print_posisiones(int nq, int *posisiones)
 }
 int is_valid(int *posisiones, int row , int col)
 {
-	int i = row - 1;
-	
-	while(i >= 0 && posisiones[i] != -1)
+	int i = row - 1; // daremos el valor de una columna menor 
+	int temp ; // declaramos un variable int temporal para poder retener y comparar despues 
+	while(i >= 0 && posisiones[i] != -1) //
 	{
-		int temp = posisiones[i];
+		temp = posisiones[i];
 		if(col == temp || col == temp + (row - i) || col == temp -(row - i))
 			return(0);
 		i--;
@@ -42,8 +42,8 @@ int is_valid(int *posisiones, int row , int col)
 
 void solve(int nq, int  *posisiones, int row)
 {
-	int col = 0; // posiscion columnas
-	while(col < nq)
+	int col = 0; // declaramos 
+	while(col < nq) // bucle comparando columnas y numero de reinas 
 	{
 		if(is_valid(posisiones,row , col))
 		{
