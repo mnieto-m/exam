041ca82f46833276a3f88bf75aa6ad1ae6dd1fad
mnieto-m <majanoyieto@gmail.com>
2026-06-26 22:26:12 +0200

    incomplete vbc

--- exam_04/lvl_1/sandbox/sandbox.c
+++ exam_04/lvl_1/sandbox/sandbox.c
@@ -1,14 +1,72 @@
-#include <stdbool.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <stdio.h>
-#include <signal.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+void alarm_handler(int sig)
+{
+	(void)sig;
+}
 
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
+	struct sigaction sa;
+	pid_t pid;
+	int status;
 
+	sa.sa_handler = alarm_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGALRM, &sa , NULL);
+
+	pid = fork();
+	if(pid == -1)
+		return(-1);
+	if(pid  == 0)
+	{
+		f();
+		exit(0);
+	}
+
+	alarm(timeout);
+
+	if(waitpid(pid, &status,0) == -1)
+	{
+		if(errno == EINTR)
+		{
+			kill(pid, SIGKILL);
+			waitpid(pid, NULL, 0);
+			if(verbose)
+				printf("Bad function: timed out after %u seconds\n", timeout);
+			return(0);
+		}
+		return(-1);
+	}
+	if(WIFEXITED(status))
+	{
+		if(WEXITSTATUS(status) == 0)
+		{
+			if(verbose)
+				printf("Nice function!\n");
+			return(1);
+		}
+		else
+		{
+			if(verbose)
+				printf("Bad funtion: exited with code %d\n", WEXITSTATUS(status));
+			return(0);
+		}
+	}
+	if(WIFSIGNALED(status))
+	{
+		if(verbose)
+			printf("Bad function: %s\n", strsignal(WTERMSIG(status)));
+		return(0);
+	}
+	return(-1);
 }
--- exam_04/lvl_2/vbc/vbc.c
+++ exam_04/lvl_2/vbc/vbc.c
@@ -1,15 +1,47 @@
 #include "unistd.h"
 #include "stdio.h"
 #include "unistd.h"
+
 char *s;
 
+int ft_sum()
+{
+	int sum1 = ft_product();
+	int sum2;
+
+	while(*s == '+')
+	{
+		s++;
+		sum2 = ft_product();
+		sum1 = sum1 + sum2;
+	}
+	return(sum1);
+}
+int checker(char *str)
+{
+	int i = 0;
+	int par = 0;
+	char last = "\0";
+
+	while(str[i])
+	{
+		if(str[i] = '(')
+			par ++;
+		else if(str[i] == ')')
+			par --;
+
+	}
+}
 int main(int argc, char **argv)
 {
+	int res;
+
 	if(argc != 2)
 		return(1);
-	if(check_input(argv[1]))
+	if(checker(argv[1]))
 		return(1);
 	s = argv[1];
-	int res = ft_sum();
+
+	res = ft_sum();
 	printf("%d\n", res);
 }
